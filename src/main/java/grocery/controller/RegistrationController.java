4dc2df69f6ac32734c7ea2cb05bcad69baa64f27
tobsucht <tobiasbagg@googlemail.com>
2020-01-01 22:38:48 +0100

    Cleanup code, add button to create new lists

--- src/main/java/grocery/controller/RegistrationController.java
+++ src/main/java/grocery/controller/RegistrationController.java
@@ -5,51 +5,49 @@ import grocery.model.validation.EmailExistsException;
 import grocery.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
 
-@RestController
+@Controller
 public class RegistrationController {
 
     @Autowired
     private UserService service;
 
     @RequestMapping(value = "/user/registration", method = RequestMethod.GET)
-    public ModelAndView showRegistrationForm(WebRequest request, Model model) {
+    public String showRegistrationForm(Model model) {
         UserDto userDto = new UserDto();
         model.addAttribute("user", userDto);
-        return new ModelAndView("registration", "user", userDto);
+        return "registration";
     }
 
     @RequestMapping(value = "/user/registration", method = RequestMethod.POST)
-    public ModelAndView registerUserAccount(
+    public String registerUserAccount(
             @ModelAttribute("user") @Valid UserDto accountDto,
             BindingResult result,
-            WebRequest request,
-            Errors errors) {
+            Model model) {
         if (result.hasErrors()) {
-            return new ModelAndView("registration", "user", accountDto);
+            model.addAttribute("user", accountDto);
+            return "registration";
         }
 
-        if (createUserAccount(accountDto, result) == null) {
+        if (createUserAccount(accountDto) == null) {
             result.rejectValue("email", "message.regError");
         }
 
-        return new ModelAndView("successRegister", "user", accountDto);
+        model.addAttribute("user", accountDto);
+        return "/grocery-list/grocery-lists";
 
     }
 
-    private User createUserAccount(UserDto accountDto, BindingResult result) {
+    private User createUserAccount(UserDto accountDto) {
         try {
             return service.createUser(accountDto);
         } catch (EmailExistsException e) {
