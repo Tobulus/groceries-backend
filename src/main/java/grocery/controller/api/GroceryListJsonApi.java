1bfc81fdbb9f557c694c002e35f8c7241f99bc06
tobsucht <tobiasbagg@googlemail.com>
2020-04-17 20:36:47 +0200

    Add list creation endpoint for android

--- src/main/java/grocery/controller/api/GroceryListJsonApi.java
+++ src/main/java/grocery/controller/api/GroceryListJsonApi.java
@@ -9,9 +9,14 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class GroceryListJsonApi {
@@ -26,6 +31,21 @@ public class GroceryListJsonApi {
     public List<GroceryList> groceryLists() {
         UserPrincipal currentUser = getUserPrincipalOrThrow();
         return groceryListRepository.findByUsers(userRepository.getOne(currentUser.getUserId()));
+    }
+
+    @PostMapping(value = "/api/grocery-list/new")
+    public Map<String, String> newGroceryList(@RequestParam("name") String name) throws IOException {
+        UserPrincipal currentUser = getUserPrincipalOrThrow();
+        Map<String, String> response = new HashMap<>();
+
+        GroceryList groceryList = new GroceryList();
+        groceryList.setName(name);
+        groceryList.getUsers().add(userRepository.getOne(currentUser.getUserId()));
+
+        groceryListRepository.save(groceryList);
+        response.put("result", "success");
+        response.put("id", groceryList.getId().toString());
+        return response;
     }
 
     private UserPrincipal getUserPrincipalOrThrow() {
