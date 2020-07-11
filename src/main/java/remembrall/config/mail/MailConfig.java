1a31a288d7e8d7fbfd9836b9035beebf2c3cb425
tobsucht <tobiasbagg@googlemail.com>
2020-07-11 20:54:50 +0200

    Mailz

--- src/main/java/remembrall/config/mail/MailConfig.java
+++ src/main/java/remembrall/config/mail/MailConfig.java
@@ -3,13 +3,14 @@ package remembrall.config.mail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 /**
  * Configures mail support. Uses 'spring.mail.host' and 'spring.mail.port' from properties file for
@@ -26,24 +27,33 @@ public class MailConfig {
     @Autowired
     private Environment env;
 
-    @Value("${spring.mail.host}")
-    private String host;
-
-    @Value("${spring.mail.port}")
-    private int port;
-
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-        mailSender.setHost(env.getProperty("mail.host"));
-        mailSender.setPort(env.getProperty("mail.port", Integer.class));
-        mailSender.setUsername(env.getProperty("mail.user"));
+        String host = env.getProperty("mail.host");
+        int port;
+
+        if (env.getProperty("mail.port") != null && !env.getProperty("mail.port").isEmpty()) {
+            port = env.getProperty("mail.port", Integer.class);
+        } else {
+            port = 587;
+        }
+
+        String user = env.getProperty("mail.user");
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(user);
         mailSender.setPassword(env.getProperty("mail.password"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
 
         logger.info("Mail host: {}", host);
         logger.info("Mail port: {}", port);
-        logger.info("Mail user: {}", env.getProperty("mail.user"));
+        logger.info("Mail user: {}", user);
 
         return mailSender;
     }
